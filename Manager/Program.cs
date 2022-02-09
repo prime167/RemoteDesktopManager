fef76d175851c62207b8bc85db7b5a5142c0d463
prime167 <3722845+prime167@users.noreply.github.com>
2022-02-09 13:09:11 +0800

    namespaces

--- Manager/Program.cs
+++ Manager/Program.cs
@@ -1,18 +1,14 @@
-namespace RemoteDesktopManager
-{
-    internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main(string[] args)
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+namespace RemoteDesktopManager;
 
-            Application.Run(new FormManager() { Args = args});
-        }
+internal static class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+
+        Application.Run(new FormManager() { Args = args});
     }
 }
